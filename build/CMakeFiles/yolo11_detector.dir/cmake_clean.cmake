7332e050d70ceae1835c5c2776f03ea8031b5186
Evert van Schalkwyk <evert.vanschalkwyk@schauenburg.co.za>
2025-06-09 15:53:39 +0200

    Init commit

---
+++ build/CMakeFiles/yolo11_detector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolo11_detector.dir/src/video_inference.cpp.o"
+  "CMakeFiles/yolo11_detector.dir/src/video_inference.cpp.o.d"
+  "yolo11_detector"
+  "yolo11_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolo11_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/yolo11_detector.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for yolo11_detector.
+# This may be replaced when dependencies are built.
